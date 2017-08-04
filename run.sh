619e82ba8d8488b636f4d18c5191e3f27bbf663c
Michelle Oliveira <michelleolivcosta@gmail.com>
2017-08-03 23:53:55 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-mvn clean install;
-docker kill app;
-docker rm app;
-docker rmi miolivc/atividade-jsf;
-docker build -t miolivc/atividade-jsf .;
-docker run -p 8080:8080 -d --name app miolivc/atividade-jsf
+docker kill app banco
+docker rm app banco 
+docker rmi miolivc/app miolivc/banco
+mvn clean install
+docker build -t miolivc/banco ./banco/
+docker run -p 5432:5432 -d --name banco miolivc/banco
+docker build -t miolivc/app ./
+docker run -p 8080:8080 -d --link banco:host-database --name app miolivc/app
